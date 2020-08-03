7f7160d7efaaee68962eb3cb0fd74f579dd472db
evkrem <evkremx@gmail.com>
2020-08-04 00:28:52 +0300

    "поиск бага в промоакциях"

--- pages/product_page.py
+++ pages/product_page.py
@@ -16,12 +16,13 @@ class ProductPage(BasePage):
     def should_be_message_product_in_basket(self):
         # проверка на совпадения названия покупаемого продукта, с названием добавленного в корзину
         product = self.browser.find_element_by_tag_name("h1").text
-        by_product = self.browser.find_element_by_css_selector(".alert.alert-safe.alert-noicon.alert-success.fade.in .alertinner").text
-        assert product in by_product, "by false product"
+        by_product = self.browser.find_element_by_css_selector(".alert.alert-safe.alert-noicon.alert-success.fade.in .alertinner strong").text
+        print("product = ", product, "\n", "by_product = ", by_product)
+        assert product == by_product, "by false product"
 
     def should_be_message_cost_product(self):
         # проверка на совпадения цены покупаемого продукта, с ценой добавленного в корзину
-        cost_by_product = self.browser.find_element_by_css_selector(".alert.alert-safe.alert-noicon.alert-info.fade.in .alertinner").text
+        cost_by_product = self.browser.find_element_by_css_selector(".alert.alert-safe.alert-noicon.alert-info.fade.in .alertinner strong").text
         cost_product = self.browser.find_element_by_css_selector(".col-sm-6.product_main .price_color").text
-        assert cost_product in cost_by_product, "cost in bassket false"
+        assert cost_product == cost_by_product, "cost in bassket false"
 
--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,10 @@
 from .pages.product_page import ProductPage
 import time
+import pytest
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+@pytest.mark.parametrize('promo_offer', ["0","1", "3", "4", "5", "6", pytest.param("7", marks=pytest.mark.xfail), "8", "9"])
+def test_guest_can_add_product_to_basket(browser, promo_offer):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}" # перебираем все промоакции подставляя в ссылку лишь ее номер
     page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
     page.add_product_to_bassket()         # выполняем метод страницы - переходим на страницу логина
@@ -10,10 +12,6 @@ def test_guest_can_add_product_to_basket(browser):
     page.should_be_rigth_product_and_cost()
     time.sleep(1)
 
-
-
-#
-#
 # def test_guest_should_see_login_link(browser):
 #     link = "http://selenium1py.pythonanywhere.com/"
 #     page = MainPage(browser, link)
