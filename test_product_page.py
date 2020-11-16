a9b2f1b4f92db13d3ec63f16daeab5119400721d
evkrem <evkremx@gmail.com>
2020-11-16 13:55:33 +0200

    "последние приготовления к код-ревью"

--- test_product_page.py
+++ test_product_page.py
@@ -9,6 +9,7 @@ import time
 
 # @pytest.mark.parametrize('page',[0, 1, 2, 3, 4, 5, 6,pytest.param(7, marks=pytest.mark.xfail(reason="он и должен был упасть")),8, 9])
 # @pytest.mark.skip
+@pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1"
     # link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{page}"
@@ -50,6 +51,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 # @pytest.mark.skip
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -57,6 +59,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.go_to_login_page()
 
 # @pytest.mark.skip
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/"
     page = BasePage(browser,link)
@@ -90,6 +93,7 @@ class TestUserAddToBasketFromProductPage():
         page.add_to_basket()
         # page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1"
         page = ProductPage(browser, link)
